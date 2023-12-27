5b512bc5840f27cdda7f1ac0e21e2b56dc74d31f
Ronie-Peter <peter.sineiro@gmail.com>
2023-12-27 09:44:09 -0300

    ALteração da lógica para impedir agendamentos em data e horário menores que a data e horários atuais.

--- backend/criar_evento.php
+++ backend/criar_evento.php
@@ -1,5 +1,4 @@
 <?php
-    
     // Definição dos horários de início e fim desejados
     $startTime = strtotime('07:30');
     $endTime = strtotime('12:00');
@@ -16,7 +15,7 @@
         array_push($availableTimes, $timeSlot);
         $startTime = $endTimeSlot;
     }
-
+    
     /* Iniciando conexão com banco de dados */
     include_once "conexao.php";
     /* Iniciando sessão */
@@ -24,14 +23,18 @@
     /* Coleta os dados do evento que o modal captura e envia */
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
     /* Conversão - data/hora do formato brasileiro para o formato do Banco de Dados */
-    $data_start_conv = date("Y-m-d H:i:s", strtotime($dados['start']));
+
+    $dataHoraAgendamento = $dados['date'].' '.$dados['start'];
+    
+    $data_start_conv = date("Y-m-d H:i:s", strtotime($dataHoraAgendamento));
     // Adiciona 45 minutos ao tempo de início para obter o tempo de término
-    $data_end_conv = date("Y-m-d H:i:s", strtotime($dados['start'] . ' +45 minutes'));
+    $data_end_conv = date("Y-m-d H:i:s", strtotime($dataHoraAgendamento . ' +45 minutes'));
     /* Define o fuso horário para o fuso de Brasília - DF */
     date_default_timezone_set('America/Sao_Paulo');
     
     /* Tratamento de erro - impede horarios iguais data do fim do evento menor que a data de inicio criar um evento para antes da data atual */
-    if($data_end_conv > $data_start_conv && $data_start_conv > date('Y-m-d H:i:s') && $data_end_conv > date('Y-m-d H:i:s')){
+    //var_dump($horarioAgendamento > date('H:i')); die;
+    if($data_start_conv > date('Y-m-d')){        
         /* Cria um statement de criação do MySQL, prepara para execução e atribui os parâmetros coletados no formulário corretamente */
         $query = "INSERT INTO events (title, color, start, end, description, status, dataCadastro) VALUES (?, '#FFD700', ?, ?, ?, false, now())";
         $stmt = mysqli_prepare($conn, $query);
