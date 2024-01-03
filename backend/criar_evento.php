60738193282ed8bf3aa2191f8df2ac28b1069b72
Ronie-Peter <peter.sineiro@gmail.com>
2024-01-03 14:32:17 -0300

    Manutenção do código para impedir agendamento no sábado e domingo

--- backend/criar_evento.php
+++ backend/criar_evento.php
@@ -32,14 +32,19 @@ $data_end_conv = date("Y-m-d H:i:s", strtotime($dataHoraAgendamento . ' +45 minu
 // Define o fuso horário para o fuso de Brasília - DF
 date_default_timezone_set('America/Sao_Paulo');
 
-// Instrução que impede horarios iguais, data do fim do evento menor que a data de inicio, criar um evento para antes da data atual
-// Verifica se a data do agendamento é futura
-if ($data_start_conv > date('Y-m-d H:i:s')) {
-    if (($data_start_conv > date('Y-m-d H:i:s')) ||
-        ($data_start_conv == date('Y-m-d H:i:s') && date('H:i', strtotime($dados['start'])) > date('H:i'))
-    ) {
-        // Verifica se o agendamento é para o mesmo dia e horário é futuro
+// Verifica se a data do agendamento é um sábado ou domingo
+$diaDaSemana = date('N', strtotime($dados['date'])); // Obtém o dia da semana (1 para segunda-feira, 7 para domingo)
 
+if ($diaDaSemana >= 6) { // Se for sábado (6) ou domingo (7)
+    $retorna = [
+        'sit' => false,
+        'msg' => '<div class="alert alert-danger" role="alert">O agendamento não é permitido para sábado ou domingo.</div>'
+    ];
+} elseif ($data_start_conv > date('Y-m-d H:i:s')) {
+    // Verifica se o agendamento é para o mesmo dia e horário é futuro
+    if (($data_start_conv > date('Y-m-d')) ||
+        ($data_start_conv == date('Y-m-d') && date('H:i', strtotime($dados['start'])) > date('H:i'))
+    ) {
         // Verifica se o horário está disponível
         $checkConsultaDisponibilidade = "SELECT * FROM events WHERE ((start <= ? AND end >= ?) OR (start >= ? AND start < ?))";
         $checkStmt = mysqli_prepare($conn, $checkConsultaDisponibilidade);
@@ -48,7 +53,10 @@ if ($data_start_conv > date('Y-m-d H:i:s')) {
         mysqli_stmt_store_result($checkStmt);
 
         if (mysqli_stmt_num_rows($checkStmt) > 0) {
-            $retorna = ['sit' => false, 'msg' => '<div class="alert alert-danger" role="alert">Horário indisponível. Reserve um horário diferente.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'];
+            $retorna = [
+                'sit' => false,
+                'msg' => '<div class="alert alert-danger" role="alert">Horário indisponível. Reserve um horário diferente.</div>'
+            ];
         } else {
             // Cria um statement de criação do MySQL, prepara para execução e atribui os parâmetros coletados no formulário corretamente
             $query = "INSERT INTO events (title, color, start, end, description, status, dataCadastro) VALUES (?, '#FFD700', ?, ?, ?, false, now())";
@@ -57,22 +65,28 @@ if ($data_start_conv > date('Y-m-d H:i:s')) {
             // Executa o statement de criação do MySQL e emite um alerta na tela para função realizada com sucesso ou erro
 
             if ($stmt->execute()) {
-                $retorna = ['sit' => true, 'msg' => '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'];
-                $_SESSION['msg'] = '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>';
+                $retorna = [
+                    'sit' => true,
+                    'msg' => '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!</div>'
+                ];
+                $_SESSION['msg'] = '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!</div>';
             } else {
-                $retorna = ['sit' => false, 'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro desconhecido. Tente novamente mais tarde.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'];
+                $retorna = [
+                    'sit' => false,
+                    'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro desconhecido. Tente novamente mais tarde.</div>'
+                ];
             }
         }
     } else {
         $retorna = [
             'sit' => false,
-            'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro no agendamento! Não é possível realizar agendamentos em datas e/ou horários passados.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+            'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro no agendamento! Não é possível realizar agendamentos em datas e/ou horários passados.</div>'
         ];
     }
 } else {
     $retorna = [
         'sit' => false,
-        'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro no agendamento! Não é possível realizar agendamentos em datas e/ou horários passados.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+        'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro no agendamento! Não é possível realizar agendamentos em datas e/ou horários passados.</div>'
     ];
 }
 
