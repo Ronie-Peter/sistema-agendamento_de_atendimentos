141fa9399fc03968b342ea578b644721d966c61d
3 Sgt Peter <peter.sineiro@gmail.com>
2024-10-10 09:59:28 -0300

    Correções na lógica e requisitos para agendamentos

--- backend/criar_evento.php
+++ backend/criar_evento.php
@@ -35,7 +35,6 @@ if (!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)) {
 }
 
 /* Conversão - data/hora do formato brasileiro para o formato do Banco de Dados */
-
 $dataHoraAgendamento = $dados['date'] . ' ' . $dados['start'];
 
 $data_start_conv = date("Y-m-d H:i:s", strtotime($dataHoraAgendamento));
@@ -47,60 +46,69 @@ date_default_timezone_set('America/Sao_Paulo');
 // Verifica se a data do agendamento é um sábado ou domingo
 $diaDaSemana = date('N', strtotime($dados['date'])); // Obtém o dia da semana (1 para segunda-feira, 7 para domingo)
 
+$currentDateTime = new DateTime(); // Data e hora atual
+$agendamentoDateTime = new DateTime($data_start_conv); // Data e hora do agendamento
+
+// Calcula a diferença entre a data/hora atual e a do agendamento
+$interval = $currentDateTime->diff($agendamentoDateTime);
+$hoursDiff = ($interval->days * 24) + $interval->h; // Converte a diferença de dias e horas em horas
+
 if ($diaDaSemana >= 6) { // Se for sábado (6) ou domingo (7)
     $retorna = [
         'sit' => false,
         'msg' => '<div class="alert alert-danger" role="alert">O agendamento não é permitido para sábado ou domingo.</div>'
     ];
-} elseif ($data_start_conv > date('Y-m-d H:i:s')) {
-    // Verifica se o agendamento é para o mesmo dia e horário é futuro
-    if (
-        ($data_start_conv > date('Y-m-d')) ||
-        ($data_start_conv == date('Y-m-d') && date('H:i', strtotime($dados['start'])) > date('H:i'))
-    ) {
-        // Verifica se o horário está disponível
-        $checkConsultaDisponibilidade = "SELECT * FROM events WHERE ((start <= ? AND end >= ?) OR (start >= ? AND start < ?))";
-        $checkStmt = mysqli_prepare($conn, $checkConsultaDisponibilidade);
-        mysqli_stmt_bind_param($checkStmt, "ssss", $data_start_conv, $data_start_conv, $data_start_conv, $data_end_conv);
-        mysqli_stmt_execute($checkStmt);
-        mysqli_stmt_store_result($checkStmt);
-
-        if (mysqli_stmt_num_rows($checkStmt) > 0) {
-            $retorna = [
-                'sit' => false,
-                'msg' => '<div class="alert alert-danger" role="alert">Horário indisponível. Reserve um horário diferente.</div>'
-            ];
-        } else {
-            // Cria um statement de criação do MySQL, prepara para execução e atribui os parâmetros coletados no formulário corretamente
-            $query = "INSERT INTO events (title, color, start, end, responsible, status, ordinance, term, phone, email, dataCadastro) VALUES (?, '#FFD700', ?, ?, ?, false, ?, ?, ?, ?, now())";
-            $stmt = mysqli_prepare($conn, $query);
-            mysqli_stmt_bind_param($stmt, "ssssssss", $dados['title'], $data_start_conv, $data_end_conv, $dados['responsible'], $dados['ordinance'], $dados['term'], $dados['telefone'], $dados['email']);
-
-            // Executa o statement de criação do MySQL e emite um alerta na tela para função realizada com sucesso ou erro
-            if ($stmt->execute()) {
-                $retorna = [
-                    'sit' => true,
-                    'msg' => '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!</div>'
-                ];
-                $_SESSION['msg'] = '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!</div>';
-            } else {
-                $retorna = [
-                    'sit' => false,
-                    'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro desconhecido. Tente novamente mais tarde.' . mysqli_error($conn) . '</div>'
-                ];
-            }
-        }
-    } else {
-        $retorna = [
-            'sit' => false,
-            'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro no agendamento! Não é possível realizar agendamentos em datas e/ou horários passados.</div>'
-        ];
-    }
-} else {
+} elseif ($agendamentoDateTime <= $currentDateTime) {
+    // Impede agendamentos para horários passados
     $retorna = [
         'sit' => false,
         'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro no agendamento! Não é possível realizar agendamentos em datas e/ou horários passados.</div>'
     ];
+} elseif ($agendamentoDateTime->format('Y-m-d') == $currentDateTime->format('Y-m-d')) {
+    // Impede agendamento para o dia vigente
+    $retorna = [
+        'sit' => false,
+        'msg' => '<div class="alert alert-danger" role="alert">O agendamento para o mesmo dia não é permitido.</div>'
+    ];
+} elseif ($hoursDiff < 48) {
+    // Impede agendamento para um prazo inferior a 48 horas
+    $retorna = [
+        'sit' => false,
+        'msg' => '<div class="alert alert-danger" role="alert">O agendamento deve ser feito com pelo menos 48 horas de antecedência.</div>'
+    ];
+} else {
+    // Verifica se o horário está disponível
+    $checkConsultaDisponibilidade = "SELECT * FROM events WHERE ((start <= ? AND end >= ?) OR (start >= ? AND start < ?))";
+    $checkStmt = mysqli_prepare($conn, $checkConsultaDisponibilidade);
+    mysqli_stmt_bind_param($checkStmt, "ssss", $data_start_conv, $data_start_conv, $data_start_conv, $data_end_conv);
+    mysqli_stmt_execute($checkStmt);
+    mysqli_stmt_store_result($checkStmt);
+
+    if (mysqli_stmt_num_rows($checkStmt) > 0) {
+        $retorna = [
+            'sit' => false,
+            'msg' => '<div class="alert alert-danger" role="alert">Horário indisponível. Reserve um horário diferente.</div>'
+        ];
+    } else {
+        // Cria um statement de criação do MySQL, prepara para execução e atribui os parâmetros coletados no formulário corretamente
+        $query = "INSERT INTO events (title, color, start, end, responsible, status, ordinance, term, phone, email, dataCadastro) VALUES (?, '#FFD700', ?, ?, ?, false, ?, ?, ?, ?, now())";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "ssssssss", $dados['title'], $data_start_conv, $data_end_conv, $dados['responsible'], $dados['ordinance'], $dados['term'], $dados['telefone'], $dados['email']);
+
+        // Executa o statement de criação do MySQL e emite um alerta na tela para função realizada com sucesso ou erro
+        if ($stmt->execute()) {
+            $retorna = [
+                'sit' => true,
+                'msg' => '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!</div>'
+            ];
+            $_SESSION['msg'] = '<div class="alert alert-success" role="alert">Atendimento agendado com sucesso!</div>';
+        } else {
+            $retorna = [
+                'sit' => false,
+                'msg' => '<div class="alert alert-danger" role="alert">Ocorreu um erro desconhecido. Tente novamente mais tarde.' . mysqli_error($conn) . '</div>'
+            ];
+        }
+    }
 }
 
 header('Content-Type: application/json');
